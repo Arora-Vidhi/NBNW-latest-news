edfb8a9069d770905aaa3cd808f70fc8e24ab9cf
Arora-Vidhi <vidarora05@gmail.com>
2024-03-02 20:44:07 +0530

    Added loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -2,10 +2,28 @@ import React, { Component } from 'react'
 
 export class Loader extends Component {
 	render() {
+		let screenSize = {
+			height: "100vh",
+			width: "100%"
+		}
+		let loaderStyles = {
+			height: "100px",
+			width: "100px",
+			borderRadius: "50%",
+			borderTop: "10px solid #d0e1fc",
+			position: 'absolute',
+			top: "50%",
+			left: "50%",
+			transform: "translate(-50%, -50%)",
+			animation: "spin 2s linear infinite"
+		}
     	return (
-    		<div><h1>Loader</h1>
-    	    	{console.log("Loader")}
-    	  	</div>
+    		<>
+				<div className="fullscreen" style={screenSize}>
+					<style>{`@keyframes spin {0% {transform: rotate(0deg);}100% {transform: rotate(360deg);}}`}</style>
+					<div className="loader" style={loaderStyles}></div>
+				</div>
+			</>
     	)
 	}
 }
